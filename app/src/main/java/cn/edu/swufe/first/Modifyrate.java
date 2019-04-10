ac2fb0d2634d49736fea6896da2e750c3588f504
Lyz <1261951967@qq.com>
2019-04-10 21:06:56 +0800

    修改汇率、设置菜单

---
+++ app/src/main/java/cn/edu/swufe/first/Modifyrate.java
@@ -0,0 +1,68 @@
+package cn.edu.swufe.first;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Modifyrate extends AppCompatActivity {
+
+    public final String TAG = "Modifyrate";
+
+    TextView btn;
+
+    EditText dollarText;
+    EditText euroText;
+    EditText wonText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifyrate);
+
+        Intent i = getIntent();
+        float dollar2=i.getFloatExtra("dollar_rate_key",0.0f);
+        float euro2=i.getFloatExtra("euro_rate_key",0.0f);
+        float won2=i.getFloatExtra("won_rate_key",0.0f);
+
+        Log.i(TAG,"onCreate:dollar2="+dollar2);
+        Log.i(TAG,"onCreate:euro2="+euro2);
+        Log.i(TAG,"onCreate:won2="+won2);
+
+        dollarText = (EditText)findViewById(R.id.dollar);
+        euroText = (EditText)findViewById(R.id.euro);
+        wonText = (EditText)findViewById(R.id.won);
+
+        dollarText.setText(String.valueOf(dollar2));
+        euroText.setText(String.valueOf(euro2));
+        wonText.setText(String.valueOf(won2));
+
+    }
+    public void save(View btn){
+        Log.i(TAG,"save：获取到新的值");
+        //获取新的输入数据
+        float newDollar = Float.parseFloat(dollarText.getText().toString());
+        float newEuro = Float.parseFloat(euroText.getText().toString());
+        float newWon = Float.parseFloat(wonText.getText().toString());
+
+        Log.i(TAG,"save：获取到新的值");
+        Log.i(TAG,"onCreate:dollar2="+newDollar);
+        Log.i(TAG,"onCreate:euro2="+newEuro);
+        Log.i(TAG,"onCreate:won2="+newWon);
+
+        //保存到Bundle
+        Intent intent = getIntent();
+        Bundle bdl = new Bundle();
+        bdl.putFloat("key_dollar",newDollar);
+        bdl.putFloat("key_euro",newEuro);
+        bdl.putFloat("key_won",newWon);
+        intent.putExtras(bdl);
+        setResult(2,intent);
+
+        //返回到调用页面
+        finish();
+    }
+}
--- app/src/main/java/cn/edu/swufe/first/RateActivity.java
+++ app/src/main/java/cn/edu/swufe/first/RateActivity.java
@@ -2,8 +2,11 @@ package cn.edu.swufe.first;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,6 +15,11 @@ import android.widget.Toast;
 import java.math.BigDecimal;
 
 public class RateActivity extends AppCompatActivity {
+
+    private final String TAG ="Rate";
+    private float dollarRate=0.1f;
+    private float euroRate=0.2f;
+    private float wonRate=0.3f;
 
     EditText rmb;
     TextView show;
@@ -37,19 +45,64 @@ public class RateActivity extends AppCompatActivity {
 
         float val;
         if (btn.getId() == R.id.btn_dollar) {
-           val = r * (1/6.7f);
+           val = r * (dollarRate);
         } else if (btn.getId() == R.id.btn_euro) {
-            val = r * (1/11f);
+            val = r * (euroRate);
         } else {
-            val = r * 500;
+            val = r * wonRate;
         }
         BigDecimal b  =   new BigDecimal(val);
         float   f   =  b.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
         show.setText(String.valueOf(f));
     }
     public void openOne(View btn){
+        openConfig();
+    }
+
+    private void openConfig() {
         Log.i("open","openOne:");
-        Intent hello = new Intent(this,Main2Activity.class);
-        startActivity(hello);
+        Intent hello = new Intent(this,Modifyrate.class);
+        hello.putExtra("dollar_rate_key",dollarRate);
+        hello.putExtra("euro_rate_key",euroRate);
+        hello.putExtra("won_rate_key",wonRate);
+
+        Log.i(TAG,"openOne:dollarRate="+dollarRate);
+        Log.i(TAG,"openOne:euroRate="+euroRate);
+        Log.i(TAG,"openOne:wonRate="+wonRate);
+
+        startActivityForResult(hello,1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.menu_set){
+            openConfig();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==1 && resultCode==2){
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar",0.1f);
+            euroRate = bundle.getFloat("key_euro",0.1f);
+            wonRate = bundle.getFloat("key_won",0.1f);
+
+            Log.i(TAG,"onActivityResult:dollarRate"+dollarRate);
+            Log.i(TAG,"onActivityResult:euroRate"+euroRate);
+            Log.i(TAG,"onActivityResult:wonRate"+wonRate);
+
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
