c3bab5d0f2455ba8c7221c2d5f89a66efb9013be
Lyz <1261951967@qq.com>
2019-05-15 22:24:03 +0800

    列表长按、删除

--- app/src/main/java/cn/edu/swufe/first/MyList2Activity.java
+++ app/src/main/java/cn/edu/swufe/first/MyList2Activity.java
@@ -1,7 +1,9 @@
 package cn.edu.swufe.first;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,11 +24,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MyList2Activity extends ListActivity implements  Runnable, AdapterView.OnClickListener{
+public class MyList2Activity extends ListActivity implements  Runnable, AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener{
 
     private static final String TAG = "MyList2";
     Handler handler;
-    private ArrayList<HashMap<String,String>> listItems;//存放文字图片信息
+    private List<HashMap<String,String>> listItems;//存放文字图片信息
     private SimpleAdapter listItemAdapter;//适配器
 
     @SuppressLint("HandlerLeak")
@@ -46,8 +48,8 @@ public class MyList2Activity extends ListActivity implements  Runnable, AdapterV
             @Override
             public void handleMessage(Message msg) {
                 if (msg.what == 7) {
-                    List<HashMap<String, String>> list2 = (List<HashMap<String, String>>) msg.obj;
-                    listItemAdapter = new SimpleAdapter(MyList2Activity.this, list2,//listitem数据源
+                    listItems = (List<HashMap<String, String>>) msg.obj;
+                    listItemAdapter = new SimpleAdapter(MyList2Activity.this, listItems,//listitem数据源
                             R.layout.list_item,//Listitem的XML布局实现
                             new String[]{"ItemTitle", "ItemDetail"},
                             new int[]{R.id.itemTitle, R.id.itemDetail}
@@ -57,33 +59,8 @@ public class MyList2Activity extends ListActivity implements  Runnable, AdapterV
                 super.handleMessage(msg);
             }
         };
-        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i(TAG,"onItemClick: parent=" + parent);
-                Log.i(TAG,"onItemClick: view=" + view);
-                Log.i(TAG,"onItemClick: position=" + position);
-                Log.i(TAG,"onItemClick: id=" + id);
-                HashMap<String, String> map = (HashMap<String, String>) getListView().getItemAtPosition(position);
-                String titleStr = map.get("ItemTitle");
-                String detailStr = map.get("ItemDetail");
-                Log.i(TAG,"onItemClick: titleStr=" + titleStr);
-                Log.i(TAG,"onItemClick: detailStr=" + detailStr);
-
-                TextView title =  view.findViewById(R.id.itemTitle);
-                TextView detail =  view.findViewById(R.id.itemDetail);
-                String title2 = (String) title.getText();
-                String detail2 = (String) detail.getText();
-                Log.i(TAG,"onItemClick: title2=" + title2);
-                Log.i(TAG,"onItemClick: detail2=" + detail2);
-
-                //打开新的页面，传入参数
-                Intent rateCalc = new Intent(this,RateActivity.class);
-                rateCalc.putExtra("title",titleStr);
-                rateCalc.putExtra("rate",Float .parseFloat(detailStr));
-                startActivity(rateCalc);
-            }
-        });
+        getListView().setOnItemClickListener(this);
+        getListView().setOnItemLongClickListener(this);
     }
     private void initListView(){
         listItems = new ArrayList<HashMap<String, String >>();
@@ -142,7 +119,51 @@ public class MyList2Activity extends ListActivity implements  Runnable, AdapterV
     }
 
     @Override
-    public void onClick(View v) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG,"onItemClick: parent=" + parent);
+        Log.i(TAG,"onItemClick: view=" + view);
+        Log.i(TAG,"onItemClick: position=" + position);
+        Log.i(TAG,"onItemClick: id=" + id);
+        HashMap<String, String> map = (HashMap<String, String>) getListView().getItemAtPosition(position);
+        String titleStr = map.get("ItemTitle");
+        String detailStr = map.get("ItemDetail");
+        Log.i(TAG,"onItemClick: titleStr=" + titleStr);
+        Log.i(TAG,"onItemClick: detailStr=" + detailStr);
 
+        TextView title =  view.findViewById(R.id.itemTitle);
+        TextView detail =  view.findViewById(R.id.itemDetail);
+        String title2 = (String) title.getText();
+        String detail2 = (String) detail.getText();
+        Log.i(TAG,"onItemClick: title2=" + title2);
+        Log.i(TAG,"onItemClick: detail2=" + detail2);
+
+        //打开新的页面，传入参数
+        Intent rateCalc = new Intent(this,RateCalcActivity.class);
+        rateCalc.putExtra("title",titleStr);
+        rateCalc.putExtra("rate",Float .parseFloat(detailStr));
+        startActivity(rateCalc);
+    }
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+        Log.i(TAG,"onItemLongClick: 长按列表position=" + position);
+        //删除操作
+        //listItems.remove(position);
+        //listItemAdapter.notifyDataSetChanged();
+        //构造对话框
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示").setMessage("请确认是否删除当前数据").setPositiveButton("是", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i(TAG,"oncClick: 对话框事件处理");
+                listItems.remove(position);
+                listItemAdapter.notifyDataSetChanged();
+            }
+        }).setNegativeButton("否",null);
+        builder.create().show();
+
+        Log.i(TAG,"onItemLongClick: "+listItems.size());
+        return false;
     }
 }
